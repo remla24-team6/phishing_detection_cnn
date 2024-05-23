9b4164e73994e97f057141ecb3c47f37456d4912
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2024-05-23 21:35:34 +0200

    fix train metric path

--- tests/test_reproducibility.py
+++ tests/test_reproducibility.py
@@ -14,18 +14,13 @@ def test_reproducibility():
     train.train()
 
     # Read metrics from the first run
-    file_path = 'reports/metrics.json'
+    file_path = 'model/metrics.json'
     metrics_1 = read_metrics(file_path)
 
     train_accuracy_1 = metrics_1['train_accuracy']
     train_loss_1 = metrics_1['train_loss']
     val_accuracy_1 = metrics_1['val_accuracy']
     val_loss_1 = metrics_1['val_loss']
-    test_accuracy_1 = metrics_1['test_accuracy']
-    avg_precision_1 = metrics_1['avg_precision']
-    avg_recall_1 = metrics_1['avg_recall']
-    avg_f1_1 = metrics_1['avg_f1']
-    roc_auc_1 = metrics_1['roc_auc']
 
     # Second training run
     train.train()
@@ -37,22 +32,12 @@ def test_reproducibility():
     train_loss_2 = metrics_2['train_loss']
     val_accuracy_2 = metrics_2['val_accuracy']
     val_loss_2 = metrics_2['val_loss']
-    test_accuracy_2 = metrics_2['test_accuracy']
-    avg_precision_2 = metrics_2['avg_precision']
-    avg_recall_2 = metrics_2['avg_recall']
-    avg_f1_2 = metrics_2['avg_f1']
-    roc_auc_2 = metrics_2['roc_auc']
 
     # Assert that metrics are the same
     assert train_accuracy_1 == train_accuracy_2, f"Train accuracies differ: {train_accuracy_1} != {train_accuracy_2}"
     assert train_loss_1 == train_loss_2, f"Train losses differ: {train_loss_1} != {train_loss_2}"
     assert val_accuracy_1 == val_accuracy_2, f"Validation accuracies differ: {val_accuracy_1} != {val_accuracy_2}"
     assert val_loss_1 == val_loss_2, f"Validation losses differ: {val_loss_1} != {val_loss_2}"
-    assert test_accuracy_1 == test_accuracy_2, f"Test accuracies differ: {test_accuracy_1} != {test_accuracy_2}"
-    assert avg_precision_1 == avg_precision_2, f"Avg precisions differ: {avg_precision_1} != {avg_precision_2}"
-    assert avg_recall_1 == avg_recall_2, f"Avg recalls differ: {avg_recall_1} != {avg_recall_2}"
-    assert avg_f1_1 == avg_f1_2, f"Avg F1 scores differ: {avg_f1_1} != {avg_f1_2}"
-    assert roc_auc_1 == roc_auc_2, f"ROC AUCs differ: {roc_auc_1} != {roc_auc_2}"
 
 if __name__ == "__main__":
     pytest.main()
