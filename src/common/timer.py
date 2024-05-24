5e15963ac07b6f574472a3323ebc007ea8c2b949
Rody Haket <rhaket@student.tudelft.nl>
2024-05-24 09:50:58 +0200

    Added monitor test; added memory test

---
+++ src/common/timer.py
@@ -0,0 +1,45 @@
+"""
+Timer utility
+"""
+from functools import wraps
+from time import perf_counter
+from typing import Callable
+from typing import Tuple
+
+import numpy as np
+
+
+def timer(func: Callable) -> Callable:
+    """Decorator to time a function.
+
+    Args:
+        func: Function to time
+
+    Returns:
+        Function results and time (in seconds)
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = perf_counter()
+        results = func(*args, **kwargs)
+        end = perf_counter()
+        run_time = end - start
+        return results, run_time
+
+    return wrapper
+
+
+
+@timer
+def predict_with_time(model, X_test: np.array) -> Tuple[np.array]:
+    """Returns model output with the time
+
+    Args:
+        model: Model to test latency on
+        X_test: Input data
+
+    Returns:
+        Predicted values and time taken to predict it
+    """
+    return model.predict(X_test)
