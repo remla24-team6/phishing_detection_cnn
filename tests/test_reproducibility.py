9eeb15d65deee0a0f0cee4c67f8b9fb9d99159a5
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2024-05-26 12:25:40 +0200

    Added Apprx

--- tests/test_reproducibility.py
+++ tests/test_reproducibility.py
@@ -3,7 +3,7 @@ import json
 from src.features import build_features
 from src import train
 
-SKIP_REPRODUCIBILITY_TEST = True
+SKIP_REPRODUCIBILITY_TEST = False
 
 def read_metrics(file_path):
     with open(file_path, 'r') as file:
@@ -37,10 +37,9 @@ def test_reproducibility():
     val_loss_2 = metrics_2['val_loss']
 
     # Assert that metrics are the same
-    assert train_accuracy_1 == train_accuracy_2, f"Train accuracies differ: {train_accuracy_1} != {train_accuracy_2}"
-    assert train_loss_1 == train_loss_2, f"Train losses differ: {train_loss_1} != {train_loss_2}"
-    assert val_accuracy_1 == val_accuracy_2, f"Validation accuracies differ: {val_accuracy_1} != {val_accuracy_2}"
-    assert val_loss_1 == val_loss_2, f"Validation losses differ: {val_loss_1} != {val_loss_2}"
-
+    assert train_accuracy_1 == pytest.approx(train_accuracy_2, rel=1e-2), f"Train accuracies differ: {train_accuracy_1} != {train_accuracy_2}"
+    assert train_loss_1 == pytest.approx(train_loss_2, rel=1e-2), f"Train losses differ: {train_loss_1} != {train_loss_2}"
+    assert val_accuracy_1 == pytest.approx(val_accuracy_2, rel=1e-2), f"Validation accuracies differ: {val_accuracy_1} != {val_accuracy_2}"
+    assert val_loss_1 == pytest.approx(val_loss_2, rel=1e-2), f"Validation losses differ: {val_loss_1} != {val_loss_2}"
 if __name__ == "__main__":
     pytest.main()
