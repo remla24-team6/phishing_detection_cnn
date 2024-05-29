dfc23adca13941a9976ba017657e3bb91a9dc3bf
Adit Whorra <aditwhorra@gmail.com>
2024-05-29 15:23:04 +0300

    Fixed linting issues + skipped deterministic tests

--- tests/model_development/test_model_development.py
+++ tests/model_development/test_model_development.py
@@ -1,37 +1,47 @@
+import random
 import pytest
 import numpy as np
-import random
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 from src.features import build_features
 from src.train import train
 from src.test import test
 from src.common.utils import load_from_json_file
 
+SKIP_DETERMINISM_TEST = True
+NUM_DETERMINISM_RUNS = 3
+
 
 def set_seeds(seed=42):
+    """Sets all the random seeds for numpy, torch and random."""
     np.random.seed(seed)
     random.seed(seed)
     tf.random.set_seed(seed)
 
-@pytest.mark.parametrize("run", range(3))  # Test three runs for non-determinism
+
+@pytest.mark.skipif(SKIP_DETERMINISM_TEST is True, reason="Takes 3 minutes to run.")
+@pytest.mark.parametrize(
+    "run", range(NUM_DETERMINISM_RUNS)
+)  # Test three runs for non-determinism
 def test_model_non_determinism(run):
+    """Trains the model NUM_DETERMINISM_RUNS number of times with different random seeds and checks
+    if the accuracy remains in a reasonable range."""
     print(f"Running non-determinism test (run {run})")
-    set_seeds(
-        42 + run
-    )
+    set_seeds(42 + run)
     train(num_features=10)
     test()
     metrics = load_from_json_file("reports/metrics.json")
     accuracy = metrics["test_accuracy"]
-    
-    assert 0 <= accuracy <= 1.0
+
+    assert 0.7 <= accuracy <= 1.0
 
 
-@pytest.mark.parametrize("run", range(3))
+@pytest.mark.skipif(SKIP_DETERMINISM_TEST is True, reason="Takes 3 minutes to run.")
+@pytest.mark.parametrize("run", range(NUM_DETERMINISM_RUNS))
 def test_model_determinism(run):
+    """Trains the model NUM_DETERMINISM_RUNS number of times with the same random seed and checks
+    if the accuracy is roughly the same every single time."""
     print(f"Running determinism test (run {run})")
-    
+
     set_seeds(42)
 
     train(num_features=10)
@@ -53,4 +63,3 @@ if __name__ == "__main__":
     build_features()
     test_model_determinism.baseline_accuracy = None
     pytest.main()
-    pass
